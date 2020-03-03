265a277bb136be6c429c14141b718fbba079bc7e
Lincoln Junior <lincolnj@hotmail.com>
2020-03-03 06:37:57 -0300

    ps-flux - 2020-03-02

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/App";
+
+// render(<HomePage />, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
